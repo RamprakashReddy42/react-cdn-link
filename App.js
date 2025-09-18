53861b76cd2bdb534a15135d82fe230293fa425a
K RAM PRAKASH REDDY <kramprakshreddy2005@gmail.com>
2025-09-18 15:49:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React,{useState}from"react";
+import "./App.css";
+function App(){
+  const[count,setCount]=useState(0);
+  return(
+    <div className="container">
+      <h2>Counter Example using useState</h2>
+      <p>You clicked{count}times</p>
+      <button onClick={()=>setCount(count+1)}>Click Me</button>
+    </div>
+  );
+}
+export default App;
